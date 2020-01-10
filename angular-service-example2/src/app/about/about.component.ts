b5e4f2a205199d30ecdb5c1f8d59489adb1d37d0
JHscar <wogns5206@naver.com>
2020-01-10 09:16:23 +0900

    Angular 컴포넌트 주입하기

---
+++ angular-service-example2/src/app/about/about.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { LogService } from '../log.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
+  providers: [LogService]
+})
+export class AboutComponent implements OnInit {
+
+  constructor(
+    private log: LogService
+  ) { }
+
+  ngOnInit() {
+    this.log.info('About');
+  }
+
+}
